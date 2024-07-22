63fc918dd8934feb85ab4e03a04c6a9d7becd794
yucheng2207 <e0560244@u.nus.edu>
2024-07-23 06:59:24 +0800

    Add basic layout and page structure to FE

--- stocker/app/layout.tsx
+++ stocker/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Layout from "antd/es/layout/layout";
+import Menu from "antd/es/menu";
+import { Header, Content, Footer } from "antd/es/layout/layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +17,94 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const items = [{key: 0, label: "Home"}, {key: 1, label: "Stock"}]
+  
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+      <Layout>
+        <Header style={{ display: 'flex', alignItems: 'center' }}>
+          <div className="demo-logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            items={items}
+            style={{ flex: 1, minWidth: 0 }}
+          />
+        </Header>
+        <Content style={{ padding: '0 48px' }}>
+          <div
+            style={{
+              // background: colorBgContainer,
+              minHeight: 280,
+              padding: 24,
+              // borderRadius: borderRadiusLG,
+            }}
+          >
+            {children}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+      </body>
     </html>
   );
 }
+
+/*
+import React from 'react';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
+const items = new Array(15).fill(null).map((_, index) => ({
+  key: index + 1,
+  label: `nav ${index + 1}`,
+}));
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <Layout>
+      <Header style={{ display: 'flex', alignItems: 'center' }}>
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={items}
+          style={{ flex: 1, minWidth: 0 }}
+        />
+      </Header>
+      <Content style={{ padding: '0 48px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div
+          style={{
+            background: colorBgContainer,
+            minHeight: 280,
+            padding: 24,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          Content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+};
+
+export default App;
+*/
---
+++ stocker/app/stock/page.tsx
@@ -0,0 +1,13 @@
+import react from "react"
+
+const Stock: React.FC<{}> = () => {
+    return (
+      <div>
+        <h3>A Simple React Component Example with Typescript</h3>
+        <p>This component shows the Logrocket logo.</p>
+        <p>For more info on Logrocket, please visit <a href="https://logrocket.com">https://logrocket.com</a></p>
+      </div>
+    );
+  };
+  
+  export default Stock;
